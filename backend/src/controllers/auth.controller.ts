1beef4312423e6f89fd4aafa260ba92f06cd9310
Prashant Kumar Maurya <mauryaprashant252@gmail.com>
2025-12-08 01:00:07 +0530

    login and signup controller and hashing password and token generation

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,31 +1,110 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import { success, z } from "zod";
+import { generateToken } from "../utils/jwt.js";
+
+const signupSchema = z.object({
+  email: z.string(),
+  username: z.string().min(3),
+  password: z.string().min(6),
+});
+export const signUp = async (req: Request, res: Response) => {
+  try {
+    const parsed = signupSchema.safeParse(req.body);
+    if (!parsed.success) {
+      return res.status(400).json({
+        success: false,
+        errors: parsed.error,
+      });
+    }
+    const { email, username, password } = parsed.data;
+
+    const isExist = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (isExist) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist with this email ! try another one",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        name: username,
+        password: hashedPassword,
+        isAdmin: false,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ success: true, message: "user created successfully" });
+  } catch (error: any) {
+    console.error("error in signup", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Not responding",
+    });
+  }
+};
 
 
-export const signUp = async(req:Request,res:Response)=>{
+const loginSchema=z.object({
+    email:z.string(),
+    password:z.string().min(6)
+})
+export const login = async(req:Request,res:Response)=>{
     try {
-        const {email,username,password} = req.body
-
-        const isExist = await prisma.user.findUnique({
-            where:{
-                email:email
-            }
-        })
-
-        if(isExist){
+        const parsed = loginSchema.safeParse(req.body)
+        if(!parsed.success){
             return res.status(400).json({
                 success:false,
-                message:"User already exist with this email ! try another one"
+                message:parsed.error
+            })
+        }
+        const {email,password} = parsed.data
+        const user = await prisma.user.findUnique({
+            where:{
+                email
+            }
+        }) 
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"Credentials wrong"
             })
         }
 
-       const encryptedPassword = bcrypt.hash(process.env.JWT_SECRET || 'Secret',password)
-    } catch (error:any) {
-        console.error("error in signup",error)
+        const isPasswordValid = await bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(401).json({
+                success:false,
+                message:"Credentials wrong"
+            })
+        }
+        const userPayload = {
+            email:user.email,
+            name:user.name,
+            isAdmin:user.isAdmin
+        }
+        const {password:_,...userData} =user
+        const token = generateToken(userPayload)
+        return res.status(200).json({success:true,data:{
+            userData,token
+        }})
+    } catch (error) {
+        console.error("login error",error)
         return res.status(500).json({
             success:false,
-            message:"Server Not responding"
+            message:"Server not working"
         })
     }
 }
---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,25 @@
+import jwt,{SignOptions,JsonWebTokenError} from 'jsonwebtoken'
+
+interface User {
+    email:string,
+    name:string,
+    isAdmin:boolean
+}
+
+export const generateToken = (data:User)=>{
+    
+    try {
+        const secret:string = process.env.JWT_SECRET || "Secret"
+        // const options:SignOptions={expiresIn:process.env.JWT_EXPIRES_IN || "1d"}
+        const expiresIn = process.env.JWT_EXPIRES_IN as any || "1d"
+
+    
+    if (!secret) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+        return jwt.sign(data,secret,{expiresIn})
+    } catch (error) {
+        console.error("generate token error",error)
+        throw new Error("failed to generate token")
+    }
+}
