950b73019dddd6b8de092a7f976bd620e9f44710
Prashant Kumar Maurya <mauryaprashant252@gmail.com>
2025-12-12 23:25:19 +0530

    seeding products

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { seedProducts } from "../../prisma/seed.js";
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
+
+
 
 export { prisma }
