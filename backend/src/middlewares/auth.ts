b6cef6da868d0f145e1a1c430ff111abe75782da
Prashant Kumar Maurya <mauryaprashant252@gmail.com>
2026-07-14 16:38:36 +0530

    fixing bugs

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,32 +1,43 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 import { User } from "../utils/jwt.js";
 
-export interface AuthRequest extends Request{
-    user?:any
+export interface AuthRequest extends Request {
+  user?: any;
 }
 
+export const auth = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token =
+      req.headers?.authorization?.split(" ")[1] || req.cookies["access-token"];
 
-export const auth= async(req:AuthRequest,res:Response,next:NextFunction)=>{
-    try {
-        const token =  req.headers?.authorization?.split(' ')[1] || req.cookies['access-token']
-
-        if(!token){
-            return res.status(401).json({success:false,message:"You are unauthorized"})
-        }
-
-        const decoded = jwt.verify(token,process.env.JWT_ACCESS_TOKEN_SECRET as string)
-        
-        req.user = decoded
-        next()
-    } catch (error:any) {
-        if (error.name === "TokenExpiredError") {
-    return res.status(401).json({
-      success: false,
-      message: "Access token expired",
-    });
-  }
-        console.error("Something went wrong with auth middleware",error)
-        return res.status(500).json({success:false,message:"You are unauthoried"})
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "You are unauthorized" });
     }
-}
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_TOKEN_SECRET as string,
+    );
+
+    req.user = decoded;
+    next();
+  } catch (error: any) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Access token expired",
+      });
+    }
+    console.error("Something went wrong with auth middleware", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "You are unauthoried" });
+  }
+};
